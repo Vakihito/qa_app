74f7b15b32d47b8ccc51cf0485318d8d330720b4
victor.tomita <victor.tomita@birdie.ai>
2023-05-17 14:46:36 -0300

    updated localhost

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
   
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetch('http://localhost:5000/predict', {
+    fetch('http://127.0.0.1:5000/predict', {
       method: 'POST',
       body: JSON.stringify({
         context: context,
